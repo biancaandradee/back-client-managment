c83d9aeeac7774215e55333f57f612713e1a5b31
Bianca Andrade <bi_andradehandan@hotmail.com>
2022-01-04 15:22:46 -0300

    Validações cobranças

---
+++ src/validations/chargesUpdateSchema.js
@@ -0,0 +1,12 @@
+const yup = require("./yup");
+
+const chargesUpdateSchema = yup.object().shape({
+  valor: yup.number().required("O campo 'Valor' é obrigatório."),
+  data_vencimento: yup
+    .string()
+    .required("O campo 'Data de Vencimento' é obrigatório."),
+  status: yup.string().required("O campo 'Status' é obrigatório."),
+  descricao: yup.string().required("O campo 'Descrição' é obrigatório."),
+});
+
+module.exports = chargesUpdateSchema;
